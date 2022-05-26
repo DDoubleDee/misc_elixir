ea6633f6984481f021ee8edb874bbeae61949395
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2022-05-26 11:01:53 +0600

    Update easy_ets.ex

--- easy_ets.ex
+++ easy_ets.ex
@@ -34,6 +34,170 @@ defmodule EasyETS do
     end
   end
   @doc """
+  Writes given map `values` into ETS with the given `name`.
+
+  Returns `{:success, id}` where `id` is the identification number given to the map inserted into ETS.
+
+  The map `values` is checked for keys that match the ones given in the structure, and they are inserted into the map if they match the constraints given to those keys.
+
+  If some keys are missing, defaults from the structure are put in their place.
+
+  ## Example
+
+    iex> EasyETS.write(:my_ets, %{name: "Alex"})
+    {:success, 0}
+
+  """
+  def write(name, values) do
+    send(name, {:write, self(), values})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  @doc """
+  Finds the map under the given `id` in the ETS with the given `name`.
+
+  Returns `{:success, values}` on successful find.
+
+  Returns `{:fail, "Does not exist"}` if `id` does not exist.
+
+  Uses simple :ets.lookup with the given `id`.
+
+  ## Example
+
+    iex> EasyETS.read(:my_ets, 0)
+    {:success, %{name: "Alex", surname: "Drake"}}
+
+    iex> EasyETS.read(:my_ets, 1)
+    {:fail, "Does not exist"}
+
+  """
+  def read(name, id) do
+    send(name, {:read, self(), id})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  @doc """
+  Matches any keys present in `values` with those written in the structure and uses :ets.match_object() to find entries that match.
+  
+  This function can also be used to output all entries in the :ets if you pass an empty map in it.
+
+  Returns `{:success, [{entry_id, entry}]}` on successful find, with the list being sorted by id lowest to highest.
+
+  Returns `{:fail, match_map, "Does not exist"}` if :ets.match_object(match_map) returns empty list.
+
+  ## Example
+
+    iex> EasyETS.match(:my_ets, %{name: "Alex"})
+    {:success, [{0, %{name: "Alex", surname: "Drake"}}]}
+    
+    iex> EasyETS.match(:my_ets, %{})
+    {:success, [
+      {0, %{name: "Alex", surname: "Drake"}},
+      {1, %{name: "Sara", surname: "Drake"}}
+      ]}
+
+    iex> EasyETS.match(:my_ets, %{name: "John"})
+    {:fail, {:_, %{name: "John", surname: :_}}, "Does not exist"}
+
+  """
+  def match(name, values) do
+    send(name, {:match, self(), values})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  @doc """
+  Replaces values under `id` with `values` if it exists, otherwise returns error.
+
+  Returns `{:success, id}` if successful.
+
+  Returns `{:fail, "ID doesn't exist"}` if `id` does not exist.
+
+  ## Examples
+
+    iex> EasyETS.update(:my_ets, 0, %{name: "Josh"})
+    {:success, 0}
+
+    iex> EasyETS.update(:my_ets, 1, %{name: "Josh"})
+    {:fail, "ID doesn't exist"}
+
+  """
+  def update(name, id, values) do
+    send(name, {:update, self(), id, values})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  @doc """
+  Deletes values under `id` if it exists.
+
+  Returns `{:success, id}` whether or not id exists.
+
+  ## Examples
+
+    iex> EasyETS.delete(:my_ets, 0)
+    {:success, 0}
+  """
+  def delete(name, id) do
+    send(name, {:delete, self(), id})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  @doc """
+  Adds a constraint `func` under a given `key`.
+
+  Returns `{:success, {key: func}}`.
+
+  In the future, if the value under `key`, passed through `func`, returns boolean `false`, it is overwritten by the default value in structure.
+
+  Be careful! If the function returns a non boolean value, it will be treated as value to be inserted under key.
+
+  ## Examples
+
+    iex> EasyETS.add_constraint(:my_ets, :name, fn name -> is_bitstring(name) end)
+    {:success, %{name: #Function<44.65746770/1 in :erl_eval.expr/5>}}
+
+  """
+  def add_constraint(name, key, func) when is_function(func) do
+    send(name, {:add_cnstr, self(), key, func})
+    receive do
+      answer ->
+        answer
+    end
+  end
+  def add_constraint(_name, _key, _func) do
+    {:fail, "3rd value passed is not function"}
+  end
+  @doc """
+  Returns structure and constraints of the connector under `name`.
+
+  Returns `{:success, structure, constraints}`.
+
+  ## Examples
+
+    iex> EasyETS.get_info(:my_ets)
+    {:success, %{name: "John", surname: "Drake"}, %{name: #Function<44.65746770/1 in :erl_eval.expr/5>}}
+
+  """
+  def get_info(name) do
+    send(name, {:get_strct, self()})
+    receive do
+      answer ->
+        answer
+    end
+  end
+
+
+
+  @doc """
   This is not supposed to be used externally, but privating this would have broken the module.
   """
   def connector(name, structure, next_id, constraints \\ %{}) do
@@ -145,162 +309,6 @@ defmodule EasyETS do
         connector(name, structure, next_id, constraints)
     end
   end
-  @doc """
-  Writes given map `values` into ETS with the given `name`.
-
-  Returns `{:success, id}` where `id` is the identification number given to the map inserted into ETS.
-
-  The map `values` is checked for keys that match the ones given in the structure, and they are inserted into the map if they match the constraints given to those keys.
-
-  If some keys are missing, defaults from the structure are put in their place.
-
-  ## Example
-
-    iex> EasyETS.write(:my_ets, %{name: "Alex"})
-    {:success, 0}
-
-  """
-  def write(name, values) do
-    send(name, {:write, self(), values})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  @doc """
-  Finds the map under the given `id` in the ETS with the given `name`.
-
-  Returns `{:success, values}` on successful find.
-
-  Returns `{:fail, "Does not exist"}` if `id` does not exist.
-
-  Uses simple :ets.lookup with the given `id`.
-
-  ## Example
-
-    iex> EasyETS.read(:my_ets, 0)
-    {:success, %{name: "Alex", surname: "Drake"}}
-
-    iex> EasyETS.read(:my_ets, 1)
-    {:fail, "Does not exist"}
-
-  """
-  def read(name, id) do
-    send(name, {:read, self(), id})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  @doc """
-  Matches any keys present in `values` with those written in the structure and uses :ets.match_object() to find entries that match.
-
-  Returns `{:success, [{entry_id, entry}]}` on successful find, with the list being sorted by id lowest to highest.
-
-  Returns `{:fail, match_map, "Does not exist"}` if :ets.match_object(match_map) returns empty list.
-
-  ## Example
-
-    iex> EasyETS.match(:my_ets, %{name: "Alex"})
-    {:success, [{0, %{name: "Alex", surname: "Drake"}}]}
-
-    iex> EasyETS.match(:my_ets, %{name: "John"})
-    {:fail, {:_, %{name: "John", surname: :_}}, "Does not exist"}
-
-  """
-  def match(name, values) do
-    send(name, {:match, self(), values})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  @doc """
-  Replaces values under `id` with `values` if it exists, otherwise returns error.
-
-  Returns `{:success, id}` if successful.
-
-  Returns `{:fail, "ID doesn't exist"}` if `id` does not exist.
-
-  ## Examples
-
-    iex> EasyETS.update(:my_ets, 0, %{name: "Josh"})
-    {:success, 0}
-
-    iex> EasyETS.update(:my_ets, 1, %{name: "Josh"})
-    {:fail, "ID doesn't exist"}
-
-  """
-  def update(name, id, values) do
-    send(name, {:update, self(), id, values})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  @doc """
-  Deletes values under `id` if it exists.
-
-  Returns `{:success, id}` whether or not id exists.
-
-  ## Examples
-
-    iex> EasyETS.delete(:my_ets, 0)
-    {:success, 0}
-  """
-  def delete(name, id) do
-    send(name, {:delete, self(), id})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  @doc """
-  Adds a constraint `func` under a given `key`.
-
-  Returns `{:success, {key: func}}`.
-
-  In the future, if the value under `key`, passed through `func`, returns boolean `false`, it is overwritten by the default value in structure.
-
-  Be careful! If the function returns a non boolean value, it will be treated as value to be inserted under key.
-
-  ## Examples
-
-    iex> EasyETS.add_constraint(:my_ets, :name, fn name -> is_bitstring(name) end)
-    {:success, %{name: #Function<44.65746770/1 in :erl_eval.expr/5>}}
-
-  """
-  def add_constraint(name, key, func) when is_function(func) do
-    send(name, {:add_cnstr, self(), key, func})
-    receive do
-      answer ->
-        answer
-    end
-  end
-  def add_constraint(_name, _key, _func) do
-    {:fail, "3rd value passed is not function"}
-  end
-  @doc """
-  Returns structure and constraints of the connector under `name`.
-
-  Returns `{:success, structure, constraints}`.
-
-  ## Examples
-
-    iex> EasyETS.get_info(:my_ets)
-    {:success, %{name: "John", surname: "Drake"}, %{name: #Function<44.65746770/1 in :erl_eval.expr/5>}}
-
-  """
-  def get_info(name) do
-    send(name, {:get_strct, self()})
-    receive do
-      answer ->
-        answer
-    end
-  end
-
-
-
 
 
   ################################################################# Helper fns
